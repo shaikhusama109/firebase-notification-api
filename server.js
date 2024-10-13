07b7cfd369b8b80b1852c6ad7cf32a9009439c13
shaikhusama109 <shaikhusama109@gmail.com>
2024-10-13 15:17:31 +0500

    Updated the code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 // Load environment variables
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
